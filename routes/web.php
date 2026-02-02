fb031983d7b0b02cf5a93f0ddae6b7272ef58ab1
Ramazan TÜFEKÇİ <4970844+ramazantufekci@users.noreply.github.com>
2026-02-02 15:56:32 +0300

    butto add

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
 Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::post('home', 'HomeController@store')->name('home.store')
